1cffcf5d46bbf35fa3e3cc013020774a0bcd959f
Md Mhafuzul Islam <mdmhafi@login001.palmetto.clemson.edu>
2018-11-06 14:23:55 -0500

    visilization added

---
+++ helper.py
@@ -0,0 +1,69 @@
+from math import sin, cos, sqrt, atan2, radians
+import math
+
+def getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2):
+    R = 6371 # Radius of the earth in km
+    dLat = radians(lat2-lat1)
+    dLon = radians(lon2-lon1)
+    rLat1 = radians(lat1)
+    rLat2 = radians(lat2)
+    a = sin(dLat/2) * sin(dLat/2) + cos(rLat1) * cos(rLat2) * sin(dLon/2) * sin(dLon/2) 
+    c = 2 * atan2(sqrt(a), sqrt(1-a))
+    d = R * c # Distance in km
+    return d
+
+def calc_velocity(dist_km, time_start, time_end):
+    """Return 0 if time_start == time_end, avoid dividing by 0"""
+    return 3600*dist_km / (time_end - time_start)if time_end > time_start else 0
+
+def distance(origin, destination):
+    lat1, lon1 = origin
+    lat2, lon2 = destination
+    radius = 6371 # km
+
+    dlat = math.radians(lat2-lat1)
+    dlon = math.radians(lon2-lon1)
+    a = math.sin(dlat/2) * math.sin(dlat/2) + math.cos(math.radians(lat1)) \
+        * math.cos(math.radians(lat2)) * math.sin(dlon/2) * math.sin(dlon/2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+    d = radius * c * 1000.0
+
+    return d
+
+def calc_bearing(lat1,lon1,lat2,lon2):
+    """
+    Calculates the bearing between two points.
+    The formulae used is the following:
+        θ = atan2(sin(Δlong).cos(lat2),
+                  cos(lat1).sin(lat2) − sin(lat1).cos(lat2).cos(Δlong))
+    :Parameters:
+      - `pointA: The tuple representing the latitude/longitude for the
+        first point. Latitude and longitude must be in decimal degrees
+      - `pointB: The tuple representing the latitude/longitude for the
+        second point. Latitude and longitude must be in decimal degrees
+    :Returns:
+      The bearing in degrees
+    :Returns Type:
+      float
+    """
+    #if (type(pointA) != tuple) or (type(pointB) != tuple):
+    #    raise TypeError("Only tuples are supported as arguments")
+
+    lat1 = math.radians(lat1)
+    lat2 = math.radians(lat2)
+    
+    diffLong = math.radians(lon2 - lon1)
+
+    x = math.sin(diffLong) * math.cos(lat2)
+    y = math.cos(lat1) * math.sin(lat2) - (math.sin(lat1)
+            * math.cos(lat2) * math.cos(diffLong))
+
+    initial_bearing = math.atan2(x, y)
+
+    # Now we have the initial bearing but math.atan2 return values
+    # from -180° to + 180° which is not what we want for a compass bearing
+    # The solution is to normalize the initial bearing as shown below
+    initial_bearing = math.degrees(initial_bearing)
+    compass_bearing = (initial_bearing + 360) % 360
+
+    return compass_bearing
